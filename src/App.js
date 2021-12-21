fd5fb7b7b896a7b5b22c157bc2c7a10415bb6e5e
mtpham@rsupport.com <mtpham@rsupport.com>
2021-12-21 16:00:53 +0900

    code push

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div>
+          <div className="shadow-sm ...">ghghghg</div>
+          <div className="shadow ...">ghhghg</div>
+          <div className="shadow-inner ...">gccccccccccccc</div>
+          <div className="shadow-lg ...">ghgh</div>
+          <div className="shadow-xl ...">ghgh</div>
+          <div className="shadow-2xl ...">ghghg</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
